0fece7bcd733e8079444afb89cba8e7c00bfcf65
Yang Wen <kevin.wenyang@gmail.com>
2016-09-01 22:14:25 +0800

    create model Category and db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160901134415) do
+ActiveRecord::Schema.define(version: 20160901141052) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "cotent"
+    t.boolean  "is_public"
+    t.integer  "category_id"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["category_id"], name: "index_articles_on_category_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
