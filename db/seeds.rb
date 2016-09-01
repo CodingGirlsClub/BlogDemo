8a2acdf32237af259f25df24935ee5928e502c9f
Yang Wen <kevin.wenyang@gmail.com>
2016-09-01 22:20:05 +0800

    add initilized data to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.find_or_create_by(name: "Science")
+Category.find_or_create_by(name: "Technology")
+Category.find_or_create_by(name: "Engineering")
+Category.find_or_create_by(name: "Art")
+Category.find_or_create_by(name: "Math")
+Article.find_or_create_by(title: "Hello World",
+                          content: "test",
+                          is_public: true,
+                          category_id: 1,
+                          user_id: 1)
