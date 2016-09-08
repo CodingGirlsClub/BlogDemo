47d372e8c185420f612caad7ca83dc9bb364611b
Yang Wen <kevin.wenyang@gmail.com>
2016-09-08 21:26:35 +0800

    add comment form in article show page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,32 @@
 class CommentsController < ApplicationController
-def index 
-  @commentable = Article.find(params[:article_id]) 
-  @comments = @commentable.comments 
-end 
+  before_action :authenticate_user!, except: [:index]
+  before_action :load_commentable
+
+  def index
+    @comments = @commentable.comments
+  end
 
   def new
+    @comment = @commentable.comments.new
+  end
+
+  def create
+    @comment = @commentable.comments.new(comment_params)
+    @comment.user = current_user
+    if @comment.save
+      redirect_to @commentable, notice: "评论成功"
+    else
+      render :new
+    end    
+  end
+
+  private
+  def load_commentable
+    resource, id = request.path.split('/')[1,2]
+    @commentable = resource.singularize.classify.constantize.find(id)
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content, :user_id)
   end
 end
