8f7532f9c7cb9514f2cc47ade226f50a507deb65
Yang Wen <kevin.wenyang@gmail.com>
2016-09-08 21:05:39 +0800

    create controller comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'comments/index'
+
+  get 'comments/new'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'visitors#index'
